3c9a0681f3ccc65917d96044aa9544993985925e
Russ Miles <russ@atomist.com>
2017-02-28 11:46:46 +0000

    Beginning to add skeleton for usage test

--- src/test/scala/com/atomist/rug/kind/gherkin/rug/kind/gherkin/GherkinUsageTest.scala
+++ src/test/scala/com/atomist/rug/kind/gherkin/rug/kind/gherkin/GherkinUsageTest.scala
@@ -1,10 +1,36 @@
 package com.atomist.rug.kind.gherkin.rug.kind.gherkin
 
+import com.atomist.project.edit.SuccessfulModification
 import org.scalatest.{FlatSpec, Matchers}
 
 class GherkinUsageTest extends FlatSpec with Matchers {
 
-  it should "get it" in {
-    assert(Gherkin().it === "it")
-  }
+  it should "update a Gherkin feature name" in {
+//    val prog =
+//      s"""
+//         |import {ProjectEditor} from '@atomist/rug/operations/ProjectEditor'
+//         |import {Project,Gherkin} from '@atomist/rug/model/Core'
+//         |import {Editor} from '@atomist/rug/operations/Decorators'
+//         |
+//         |import {PathExpressionEngine} from '@atomist/rug/tree/PathExpression'
+//         |
+//         |@Editor("", "Update Feature Name")
+//         |class UpdateProperty {
+//         |
+//         | edit(project: Project) {
+//         |
+//         |   project.context().pathExpressionEngine().with<Gherkin>(project, `/Gherkin()`, g => {
+//         |     console.log(`Gherkin is $${g}`)
+//         |     if (p.featureName() == "pom.xml")
+//         |       p.setGroupId("mygroup")
+//         |   })
+//         | }
+//         |}
+//         |
+//        |export let pe = new UpdateProperty()
+//      """.stripMargin
+//    updateWith(prog, JavaTypeUsageTest.NewSpringBootProject) match {
+//      case success: SuccessfulModification =>
+//      case _ => ???
+    }
 }
